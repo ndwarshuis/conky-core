6b0125984df8d46122f1da3a4437d795b8066ef5
petrucci4prez <natedwarshuis@gmail.com>
2017-07-16 13:56:42 -0400

    rename temp global variable

--- super/Widget.lua
+++ super/Widget.lua
@@ -819,7 +819,7 @@ local initPlot = function(arg)
 			p2 = p2
 		},
 		seconds = seconds,
-		n = seconds * CONSTRUCTION_GLOBAL.UPDATE_INTERVAL,
+		n = seconds * G_DIMENSIONS_.UPDATE_INTERVAL,
 		fill_source = data_fill_pattern and Super.Pattern{
 			pattern = data_fill_pattern,
 			p1 = p1,

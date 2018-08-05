26bbebf6fb40c7ad4762dd0b884b85c22d3a98b4
petrucci4prez <natedwarshuis@gmail.com>
2018-08-05 14:54:24 -0400

    split compound bar into static and dynamic drawing

--- widget/poly/CompoundBar.lua
+++ widget/poly/CompoundBar.lua
@@ -9,26 +9,37 @@ local __cairo_stroke 	  		= cairo_stroke
 local __cairo_append_path  		= cairo_append_path
 
 local set = function(obj, index, percent)
-	Bar.set(obj.bars[index], percent)
+   Bar.set(obj.bars[index], percent)
 end
 
-local draw = function(obj, cr)
-	local first_bar = obj.bars[1]
-	__cairo_set_line_width(cr, first_bar.thickness)
-	__cairo_set_line_cap(cr, first_bar.cap)
-	
-	for i = 1, obj.bars.n do
-		local bar = obj.bars[i]
-		__cairo_set_source(cr, bar.source)
-		__cairo_append_path(cr, bar.path)
-		__cairo_stroke(cr)
-		__cairo_set_source(cr, bar.current_source)
-		__cairo_append_path(cr, bar.bar_path)
-		__cairo_stroke(cr)
-	end
+local draw_static = function(obj, cr)
+   local first_bar = obj.bars[1]
+   __cairo_set_line_width(cr, first_bar.thickness)
+   __cairo_set_line_cap(cr, first_bar.cap)
+   
+   for i = 1, obj.bars.n do
+	  local this_bar = obj.bars[i]
+	  __cairo_set_source(cr, this_bar.source)
+	  __cairo_append_path(cr, this_bar.path)
+	  __cairo_stroke(cr)
+   end
+end
+
+local draw_dynamic = function(obj, cr)
+   local first_bar = obj.bars[1]
+   __cairo_set_line_width(cr, first_bar.thickness)
+   __cairo_set_line_cap(cr, first_bar.cap)
+   
+   for i = 1, obj.bars.n do
+	  local this_bar = obj.bars[i]
+	  __cairo_set_source(cr, this_bar.current_source)
+	  __cairo_append_path(cr, this_bar.bar_path)
+	  __cairo_stroke(cr)
+   end
 end
 
 M.set = set
-M.draw = draw
+M.draw_static = draw_static
+M.draw_dynamic = draw_dynamic
 
 return M
